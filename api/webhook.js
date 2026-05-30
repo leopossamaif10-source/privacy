69d75f5eac704a38910bef16f1cadeab26bd785a
leopossamaif10-source <leopossamaif10@gmail.com>
2026-05-30 01:11:37 -0300

    Delete api directory

--- api/webhook.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method !== 'POST') return res.status(405).end();
-  return res.status(200).json({ ok: true });
-}
